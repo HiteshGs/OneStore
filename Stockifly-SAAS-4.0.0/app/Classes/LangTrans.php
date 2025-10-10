c580096afa98a413a203b973f5f081c77ea1faa4
prathamdigit <pratham.1digit@gmail.com>
2025-10-10 15:32:55 +0530

    button name updated

--- Stockifly-SAAS-4.0.0/app/Classes/LangTrans.php
+++ Stockifly-SAAS-4.0.0/app/Classes/LangTrans.php
@@ -1136,7 +1136,7 @@ class LangTrans
             'by_sales' => 'By Sales',
             'unit_sold' => 'Unit Sold',
             'import_products' => 'Import Products',
-            'import_products_custom_fields_master' => 'Import Master Product',
+            'import_products_using_custom_fields_master' => 'Import Master Product',
             'total_purchase_price' => 'Total Purchase Price',
             'total_sales_price' => 'Total Sales Price',
             'product_type' => 'Product Type',
