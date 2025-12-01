422524ab2d377a6c80a49df614b57616e1f5e72f
prathamdigit <pratham.1digit@gmail.com>
2025-12-01 11:52:30 +0530

    pos tax added

--- Stockifly-SAAS-4.0.0/resources/js/main/views/stock-management/pos/Pos.vue
+++ Stockifly-SAAS-4.0.0/resources/js/main/views/stock-management/pos/Pos.vue
@@ -191,18 +191,19 @@
                                                             "
                                                         />
                                                     </template>
-                                                    <template
-                                                        v-if="
-                                                            column.dataIndex ===
-                                                            'subtotal'
-                                                        "
-                                                    >
-                                                        {{
-                                                            formatAmountCurrency(
-                                                                record.subtotal
-                                                            )
-                                                        }}
-                                                    </template>
+                                                   <template
+    v-if="
+        column.dataIndex ===
+        'subtotal'
+    "
+>
+    {{
+        formatAmountCurrency(
+            record.subtotal
+        )
+    }}
+</template>
+
                                                     <template
                                                         v-if="
                                                             column.dataIndex === 'action'
@@ -594,18 +595,9 @@
                                                 @change="quantityChanged(record)"
                                             />
                                         </template>
-                                        <template
-    v-if="
-        column.dataIndex ===
-        'subtotal'
-    "
->
-    {{ formatAmountCurrency(record.subtotal) }}
-    <template v-if="record.tax_rate !== null && record.tax_rate !== undefined">
-        ({{ record.tax_rate }}%)
-    </template>
-</template>
-
+                                        <template v-if="column.dataIndex === 'subtotal'">
+                                            {{ formatAmountCurrency(record.subtotal) }}
+                                        </template>
                                         <template v-if="column.dataIndex === 'action'">
                                             <a-button
                                                 type="primary"
